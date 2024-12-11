297db45bd496dcae62ec01ee36aad00b466ddee1
Ogas-B <ogasbrian735@outlook.com>
2024-12-11 19:58:07 -0300

    Paso 1 del proyecto: 1er clase

--- Proyecto_Pagina/Proyecto_Pagina/urls.py
+++ Proyecto_Pagina/Proyecto_Pagina/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+from .views import saludo, dia_de_hoy, muestra_nombre, probando_template
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
+    path('diaDeHoy/', dia_de_hoy),
+    path('muestra_nombre/<nombre>/', muestra_nombre),
+    path('probando_template/', probando_template),
 ]
+
